0c8367c73d24c048f0e6d77d2f516329d44c67dd
Abhishek <eduandabhi@gmail.com>
2022-06-13 16:50:00 +0530

    Proxy Pattern

---
+++ proxy-pattern/proxyPattern.js
@@ -0,0 +1,27 @@
+const person = {
+  name: "John Doe",
+  age: 42,
+  nationality: "American",
+};
+
+const personProxy = new Proxy(person, {
+  get: (obj, prop) => {
+    if (!obj[prop]) {
+      console.log(
+        "hmm this property does not seem to exists on the target object"
+      );
+    } else console.log(`The valus of ${prop} is ${obj[prop]}`);
+  },
+  set: (obj, prop, value) => {
+    if (prop === "age" && typeof value !== "number") {
+      console.log("Sorry you can only pass numeric values for age");
+    } else if (prop === "name" && value.length < 2) {
+      console.log("You need to provide a valid name.");
+    } else {
+      console.log(`Changed ${prop} form ${obj[prop]} to ${value}`);
+      obj[prop] = value;
+    }
+  },
+});
+personProxy.name = "Abhishek";
+personProxy.age = 34;
