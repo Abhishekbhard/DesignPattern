730cf3a0c819fe8dd1a29c91dc93359ecabeb5f0
Abhishek <eduandabhi@gmail.com>
2022-06-24 23:42:00 +0530

    Mixin inheritance

--- Mixin-Pattern/mixin.js
+++ Mixin-Pattern/mixin.js
@@ -3,14 +3,25 @@ class Dog {
     this.name = name;
   }
 }
-
+const animalFunctionality = {
+  walk: () => console.log("Walking!"),
+  sleep: () => console.log("Sleeping"),
+};
 const dogFunctionality = {
   bark: () => console.log(`Wofff....`),
   wagTail: () => console.log(`Waging my tail!`),
   play: () => console.log("playing"),
+  walk() {
+    super.walk();
+  },
+  sleep() {
+    super.sleep();
+  },
 };
+Object.assign(dogFunctionality, animalFunctionality);
 Object.assign(Dog.prototype, dogFunctionality);
 
 const pet1 = new Dog("Daisy");
 pet1.bark();
 pet1.wagTail();
+pet1.sleep();
