14abaf21c2c3718b19e09f212822fd1a64368361
Abhishek <eduandabhi@gmail.com>
2022-07-12 21:36:51 +0530

    Uplifting state

--- Render-Prop-Pattern/temperature-converter/src/App.js
+++ Render-Prop-Pattern/temperature-converter/src/App.js
@@ -1,12 +1,34 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
-function App() {
+function Input({ value, handleChange }) {
   return (
-    <div className="App">
-      <h1>☃️ Temperature Converter 🌞</h1>
-    </div>
+    <input
+      type="text"
+      value={value}
+      onChange={(e) => handleChange(e.target.value)}
+      placeholder="Temp in °C"
+    />
   );
 }
 
+function App() {
+  const [value, setValue] = useState("");
+
+  return (
+    <div className="App">
+      <h1>☃️ Temperature Converter 🌞</h1>
+      <Input value={value} handleChange={setValue} />
+      <Kelvin value={value} />
+      <Fahrenheit value={value} />
+    </div>
+  );
+}
+function Kelvin({ value = 0 }) {
+  return <div className="temp">{value + 273.15}K</div>;
+}
+
+function Fahrenheit({ value = 0 }) {
+  return <div className="temp">{(value * 9) / 5 + 32}°F</div>;
+}
 export default App;
